fcfa846bc9a294b85c6d917b5a03258915e99007
ben-pulido <ben@benpulido.com>
2022-02-08 18:12:09 -0500

    Teams t2t scripts

--- M365GraphAdmin.psm1
+++ M365GraphAdmin.psm1
@@ -163,10 +163,18 @@ function Get-GraphUser {
 }
 function Get-GraphUserEvents {
     param (
-        [Parameter(Mandatory = $True)]$UserPrincipalName
+        [Parameter(Mandatory = $True)]$UserPrincipalName,
+        [Parameter(Mandatory = $False)]$Filter
     )
-    $URI = "https://graph.microsoft.com/$GraphVersion/users/$userprincipalname/events"
-    Get-NextPage -URI $URI
+    if ($filter){
+        
+        $URI = "https://graph.microsoft.com/$GraphVersion/users/$userprincipalname/events?`$filter=$filter"
+        Get-NextPage -URI $URI
+    }
+    else {
+        $URI = "https://graph.microsoft.com/$GraphVersion/users/$userprincipalname/events"
+        Get-NextPage -URI $URI
+    }
 }
 function Set-GraphUser {
     [CmdletBinding()]

cb3718f60ceef15d13c11a2f91dc3b3d6fc4023a
ben-pulido <ben@benpulido.com>
2022-02-09 23:56:41 -0500

    convert-oguserevent complet

--- M365GraphAdmin.psm1
+++ M365GraphAdmin.psm1
@@ -219,8 +219,8 @@ function Convert-OGUserEvent {
     }
     if ($Event.end) {
         $end = [PSCustomObject]@{
-            dateTime = $Content.end.dateTime
-            timeZone = $Content.end.timeZone
+            dateTime = $Event.end.dateTime
+            timeZone = $Event.end.timeZone
         }
         $body.end = $end
     }
@@ -268,14 +268,95 @@ function Convert-OGUserEvent {
     }
     $account_params = @{
         Headers     = @{Authorization = "Bearer $($GraphAPIKey)" }
-        Uri         = "https://graph.microsoft.com/$GraphVersion/users/$($Event.organizer.address)/events"
+        Uri         = "https://graph.microsoft.com/$GraphVersion/users/$($Event.organizer.emailaddress.address)/events"
         body        = $Body | ConvertTo-Json -Depth 10
         Method      = 'POST'
         ContentType = 'application/json'
     }
     Invoke-RestMethod @Account_params
 }
-    
+function Convert-OGGroupEvent {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)][PSObject]$Event
+    )
+    $Body = @{}
+    if ($Event.subject) {
+        $body.subject = $Event.subject
+    }
+    if ($Event.body.content) {
+        $updateContent = Remove-OGTeamsEventInfo -html $Event.body.content
+        $bodyContent = [PSCustomObject]@{
+            contentType = "HTML"
+            content     = $updateContent
+        }
+        $body.body = $bodycontent
+    }
+    if ($Event.start) {
+        $start = [PSCustomObject]@{
+            dateTime = $Event.start.dateTime
+            timeZone = $Event.start.timeZone
+        }
+        $body.start = $start
+    }
+    if ($Event.end) {
+        $end = [PSCustomObject]@{
+            dateTime = $Event.end.dateTime
+            timeZone = $Event.end.timeZone
+        }
+        $body.end = $end
+    }
+    if ($event.recurrence) {
+        $recurrence = [PSCustomObject]@{
+            pattern = [PSCustomObject]@{
+                type       = $event.recurrence.pattern.type
+                interval   = $event.recurrence.pattern.interval
+                daysOfWeek = @($event.recurrence.pattern.daysofweek)
+            }
+            range   = [PSCustomObject]@{
+                type      = $event.recurrence.range.type
+                startDate = $event.recurrence.range.startDate
+            }
+        }
+        if ($recurrence.range.type -ne "noEnd") {
+            $recurrence.range = Add-Member -MemberType NoteProperty  -Name 'endDate' -Value $event.recurrence.range.endDate
+        }
+        $body.recurrence = $recurrence
+    }
+    if ($event.location.displayName) {
+        $location = [PSCustomObject]@{
+            displayName = $event.location.displayName
+        }
+        $body.location = $location
+    }
+    if ($event.attendees) {
+        $array = @(
+            foreach ($attendeeItem in $event.attendees) {
+                $attendees = @{}
+                $emailAddress = [PSCustomObject]@{
+                    address = $attendeeItem.emailAddress.address
+                    name    = $attendeeItem.emailAddress.name
+                }
+                $attendees.emailaddress = $emailAddress
+                $type = $attendeeItem.type
+                $attendees.type = $type
+                $attendees
+            })
+        $body.attendees = $array
+    }
+    if ($event.allowNewTimeProposals) {
+        $allowNewTimeProposals = $event.allowNewTimeProposals
+        $body.allowNewTimeProposals = $allowNewTimeProposals
+    }
+    $account_params = @{
+        Headers     = @{Authorization = "Bearer $($GraphAPIKey)" }
+        Uri         = "https://graph.microsoft.com/$GraphVersion/groups/$($Event.organizer.emailaddress.address)/events"
+        body        = $Body | ConvertTo-Json -Depth 10
+        Method      = 'POST'
+        ContentType = 'application/json'
+    }
+    Invoke-RestMethod @Account_params
+}
 #$Body = new-object -name body -Property $Body -TypeName psobject
 
 

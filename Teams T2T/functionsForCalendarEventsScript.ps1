73b4a56217f1d26a824697f6fccc21afbdb8f226
ben-pulido <ben@benpulido.com>
2022-02-13 13:16:15 -0500

    Convert Event Loggin contd.

--- Teams T2T/functionsForCalendarEventsScript.ps1
+++ Teams T2T/functionsForCalendarEventsScript.ps1
@@ -185,3 +185,37 @@ function Convert-OGUserEvent {
     }
     Invoke-RestMethod @Account_params
 }
+Function Write-ConvertEventLog {
+    [CmdletBinding()]
+    Param(
+    [Parameter(Mandatory=$False)]
+    [ValidateSet("INFO","WARN","ERROR","FATAL","DEBUG")]
+    [String]
+    $LogType,
+
+    [Parameter(Mandatory=$True)]
+    [string]
+    $User,
+
+    [Parameter(Mandatory=$True)]
+    [string]
+    $EventId,
+
+    [Parameter(Mandatory=$True)]
+    [string]
+    $Message,
+
+    [Parameter(Mandatory=$False)]
+    [string]
+    $LogPath
+    )
+
+    $logObject = [PSCustomObject]@{
+        Time = $((Get-Date).toString("yyyy/MM/dd HH:mm:ss"))
+        LogType = $LogType
+        UPN = $User
+        EventId = $EventId
+        Message = $Message
+    }
+    Export-Csv -Path $LogPath -InputObject $logObject -NoTypeInformation -Append
+}

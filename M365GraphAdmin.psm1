71cc57e63e9f93d5cfcc882a4d67cfe3e2e2c685
ben-pulido <ben@benpulido.com>
2022-04-15 12:14:05 -0400

    Get-OGUserSeriesInstance

--- M365GraphAdmin.psm1
+++ M365GraphAdmin.psm1
@@ -177,6 +177,19 @@ function Get-OGUserEvents {
         Get-OGNextPage -URI $URI
     }
 }
+
+function Get-OGUserSeriesInstance {
+    param (
+        [Parameter(Mandatory = $True)]$UserPrincipalName,
+        [Parameter(Mandatory = $True)]$EventId,
+        [Parameter(Mandatory = $True)][dateTime]$StartDateTime,
+        [Parameter(Mandatory = $True)][datetime]$EndDateTime
+    )
+    $StartDateTime = $StartDateTime.ToString("o")
+    $EndDateTime = $EndDateTime.ToString("o")
+        $URI = "https://graph.microsoft.com/$GraphVersion/users/$userprincipalname/events/$EventId/instances?startDateTime=$StartDateTime&endDateTime=$EndDateTime"
+        Get-OGNextPage -URI $URI
+}
 function Get-OGGroupEvents {
     param (
         [Parameter(Mandatory = $True)]$GroupId,

df46d64db61aacda9ee55313e59684fef3eaf108
clouddots <clouddots>
2022-05-19 19:28:10 -0400

    get-oguserdrive

--- OGraph/functions/Get-OGUserDrive.ps1
+++ OGraph/functions/Get-OGUserDrive.ps1
@@ -1,4 +1,33 @@
+<#
+.SYNOPSIS
+Get user OneDrive Meta information
+
+.DESCRIPTION
+Long description
+
+.PARAMETER UserPrincipalName
+Userprincipalname or ID for lookup
+
+.EXAMPLE
+Get-OGUserDrive -userprincipalname test.user@domain.com
+.NOTES
+General notes
+#>
 Function Get-OGUserDrive {
+    
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]$UserPrincipalName
+    )
+    if ($UserPrincipalName) {
+        $account_params = @{
+            URI        = "https://graph.microsoft.com/$GraphVersion/users/$userprincipalname/drive"
+            Method     = 'GET'
+            outputtype = 'psobject'
+        }
+        Invoke-GraphRequest @Account_params
+    }
+}Function Get-OGUserDrive {
     
     [CmdletBinding(DefaultParameterSetName = 'UPN')]
     param (

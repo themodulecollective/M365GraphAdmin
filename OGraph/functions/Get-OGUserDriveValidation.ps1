190bb11dbeea83b33b84bfab3c2f56561503b3db
clouddots <clouddots>
2022-05-20 16:58:51 -0400

    update

--- OGraph/functions/Get-OGUserDriveValidation.ps1
+++ OGraph/functions/Get-OGUserDriveValidation.ps1
@@ -1,17 +1,17 @@
 function Get-OGUserDriveValidation {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $False, ParameterSetName = 'multiUser')]
+        [Parameter(Mandatory = $False, ParameterSetName = 'multiUser')][psobject[]]
         $SourceData,
-        [Parameter(Mandatory = $False, ValueFromPipelineByPropertyName, ParameterSetName = 'singleUser')]
+        [Parameter(Mandatory = $False, ValueFromPipelineByPropertyName, ParameterSetName = 'singleUser')][string]
         $UserPrincipalName,
-        [Parameter(Mandatory = $false, ParameterSetName = 'multiUser')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'multiUser')][string]
         $UPNHeader,
         [Parameter(Mandatory, ParameterSetName = 'multiUser')]
         [Parameter(Mandatory, ParameterSetName = 'singleUser')]
         $TenantName,
         [Parameter(Mandatory = $false)]$ExportPath,
-        [Parameter(Mandatory, ParameterSetName = 'multiUser')][Switch]$ValidateFirstTen,
+        [Parameter(Mandatory, ParameterSetName = 'multiUser')][Int]$ValidationLimit,
         [Switch]$PnPPowershell
         
     )
@@ -19,33 +19,33 @@ function Get-OGUserDriveValidation {
     begin {
         $TenantName = $TenantName.tolower()
         $TenantURL = "https://$($tenantName)-my.sharepoint.com/personal/*"
-        if ($ValidateFirstTen) {
-            $firstTen = 0..9
-        }
-        else {
-            $firstTen = 0..($SourceData.length)
+        switch ($PSBoundParameters.Keys -contains 'ValidationLimit') {
+            $true {
+                $limit = $ValidationLimit - 1
+                $SourceData = $SourceData[0..$limit]
+            }
         }
     }
 
     process {
         if ($PSCmdlet.ParameterSetName -eq 'multiUser') {
-            $results = @($SourceData[$firstTen].foreach({
-                        if ($PnPPowershell) {
-                            $target = Get-PnPUserProfileProperty -Account $psitem.$UPNHeader
-                            $target = $target.personalurl
-                        }
-                        else {
-                            $target = Get-OGUserDrive -UserPrincipalName $psitem.$UPNHeader
-                            $target = $target.weburl
-                        }
-                        if ($target -like $TenantURL) {
-                            $_ | Select-Object *, @{Name = 'targetODurl'; Expression = { $target } }
-                        }
-                        else {
-                            $_ | Select-Object *, @{Name = 'targetODurl'; Expression = { $null } }
-                        }
-                        $target = $null
-                    }))
+            $results = @(foreach ($item in $SourceData) {
+                    if ($PnPPowershell) {
+                        $target = Get-PnPUserProfileProperty -Account $item.$UPNHeader
+                        $target = $target.personalurl
+                    }
+                    else {
+                        $target = Get-OGUserDrive -UserPrincipalName $item.$UPNHeader
+                        $target = $target.weburl
+                    }
+                    if ($target -like $TenantURL) {
+                        $_ | Select-Object *, @{Name = 'targetODurl'; Expression = { $target } }
+                    }
+                    else {
+                        $_ | Select-Object *, @{Name = 'targetODurl'; Expression = { $null } }
+                    }
+                    $target = $null
+                })
         }
         else {
             $target = Get-OGUserDrive -UserPrincipalName $UserPrincipalName

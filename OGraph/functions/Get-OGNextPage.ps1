4459cb5178c88245015e511d2e37c6756ea5f404
clouddots <ben@clouddots.io>
2022-05-26 20:31:44 -0400

    update get-nextpage

--- OGraph/functions/Get-OGNextPage.ps1
+++ OGraph/functions/Get-OGNextPage.ps1
@@ -1,29 +1,29 @@
-<#
-    .SYNOPSIS
-    Provide paginiation functionality on all functions
-    
-    .DESCRIPTION
-    Long description
-    
-    .PARAMETER URI
-    URI passed from parent function
-    example: get-oguser would pass https://graph.microsoft.com/$GraphVersion/users
-    
-    .PARAMETER SearchDisplayName
-    Allows additional header information for search functionality on some parent functions
-    
-    .PARAMETER PSObject
-    Outputs response as PSobject instead of HashTable
-
-    .EXAMPLE
-    An example
-    
-    .NOTES
-    General notes
-    #>
 Function Get-OGNextPage {
+    
     [CmdletBinding()]
     param (
+        [Parameter(Mandatory = $True)][string]$URI,
+        [Switch]$SearchDisplayName
+    )
+    $account_params = @{
+        Headers     = @{Authorization = "Bearer $($GraphAPIKey)" }
+        URI         = $URI
+        Method      = 'GET'
+        ContentType = 'application/json'
+    }
+    if ($SearchDisplayName) {
+        $account_params.headers.add('ConsistencyLevel', 'eventual')
+    }
+    $Result = Invoke-RestMethod @Account_params
+    if ($result.'@odata.nextlink') {
+        Get-OGNextPage -Uri $result.'@odata.nextlink'
+    }
+    elseif (!$result.'@odata.nextlink') {
+        $Result.Value
+    }
+
+}
+
         [Parameter(Mandatory = $True)][string]$URI,
         [Switch]$SearchDisplayName
     )
@@ -37,10 +37,10 @@ Function Get-OGNextPage {
         $account_params.headers.add('ConsistencyLevel', 'eventual')
     }
     $Result = Invoke-GraphRequest @Account_params
-    if ($results.'@odata.nextlink') {
-        Get-OGNextPage -Uri $results.'@odata.nextlink'
+    if ($result.'@odata.nextlink') {
+        Get-OGNextPage -Uri $result.'@odata.nextlink'
     }
-    elseif (!$results.'@odata.nextlink') {
+    elseif (!$result.'@odata.nextlink') {
         $Result.Value
     }
 }
